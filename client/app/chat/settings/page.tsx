26dd0444baa3e29cc05748ddcc157e5d41658810
Sergy Yakovenko <sergeyyakovenko15@gmail.com>
2024-05-12 15:53:29 +0300

    updated settings

--- client/app/chat/settings/page.tsx
+++ client/app/chat/settings/page.tsx
@@ -3,6 +3,19 @@ import { GrUserSettings } from "react-icons/gr";
 import { IoColorPaletteOutline } from "react-icons/io5";
 
 export default function Page() {
+  const SETTING_ITEMS = [
+    {
+      label: "Настройки профиля",
+      icon: <GrUserSettings size={20} />,
+      onClick: () => {},
+    },
+    {
+      label: "Оформление",
+      icon: <IoColorPaletteOutline size={25} />,
+      onClick: () => {},
+    },
+  ];
+
   return (
     <div className={"text-white"}>
       <div className={"mt-5 text-center text-2xl font-bold mx-auto"}>
@@ -15,23 +28,20 @@ export default function Page() {
           }
         />
       </div>
-      <div className={"mt-14 flex flex-col gap-2"}>
-        <div
-          className={
-            "flex gap-4 items-center p-2 hover:bg-slate-100/10 cursor-pointer rounded-md"
-          }
-        >
-          <GrUserSettings size={20} />
-          Настройки профиля
-        </div>
-        <div
-          className={
-            "flex gap-4 items-center p-2 hover:bg-slate-100/10 cursor-pointer rounded-md"
-          }
-        >
-          <IoColorPaletteOutline size={25} />
-          Оформление
-        </div>
+      <p className={"mt-4 text-center"}>sergeyyskovenko15@gmail.com</p>
+
+      <div className={"mt-8 flex flex-col gap-2"}>
+        {SETTING_ITEMS.map((data) => (
+          <div
+            key={data.label}
+            className={
+              "flex gap-4 items-center p-2 bg-slate-300/10 hover:bg-slate-100/20 active:bg-slate-100/30 cursor-pointer rounded-md select-none"
+            }
+          >
+            {data.icon}
+            {data.label}
+          </div>
+        ))}
       </div>
     </div>
   );
